63d0e7448160a0b0429e3473d8e85e2e0c7ef014
prizark <ozghayyer11@gmail.com>
2022-03-31 23:27:32 -0700

    Initial commit

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Press enter to continue"));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+    FString Hiddenword = TEXT("cake");
+    PrintLine(Input);
+
+}
